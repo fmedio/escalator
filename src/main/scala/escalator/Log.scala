89a350d38f69f95feff2d297909b0dab8ff425ab
U-freedom\Fabrice <Fabrice@freedom.(none)>
2012-04-30 17:46:03 -0700

    Mavenize all the thinks

--- src/main/scala/escalator/Log.scala
+++ src/main/scala/escalator/Log.scala
@@ -1,6 +1,10 @@
 package escalator
 
 object Log {
+  def error(source: Object, message: String, throwable: Throwable) {
+
+  }
+
   def info(source: Object, message: String, throwable: Throwable) {
 
   }
